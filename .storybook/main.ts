133d1bb9deb483591a0e569c4d8fb13744662867
Gabriel <oi@gabrielteodoro.com>
2023-07-04 10:35:56 -0300

    feat: hold storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,35 +1,30 @@
-import { resolve as pathResolve } from 'path'
+import path from 'path'
 
-import type { StorybookConfig } from '@storybook/nextjs'
-
-const resolve = (path: string) => pathResolve(__dirname, path)
-
+import type { StorybookConfig } from "@storybook/nextjs";
 const config: StorybookConfig = {
-  staticDirs: [resolve('../public')],
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
   ],
   framework: {
-    name: '@storybook/nextjs',
+    name: "@storybook/nextjs",
     options: {},
   },
   docs: {
-    autodocs: 'tag',
+    autodocs: "tag",
   },
-  core: {
-    disableTelemetry: true,
+  features: {
+    storyStoreV7: true,
   },
   webpackFinal: (config) => {
     // @ts-ignore
     config.resolve.alias = {
       ...config.resolve?.alias,
-      '@': [resolve('../src/'), resolve('../')],
+      '@': [path.resolve('../src/'), path.resolve('../')],
     }
     return config
   },
-}
-
-export default config
+};
+export default config;
